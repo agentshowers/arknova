2708e0fe68af1c4e654fa974e0c9f87ed28ac503
Joao Amorim <i.psyduck@gmail.com>
2024-04-15 17:23:21 +0100

    make curl silent

--- script.sh
+++ script.sh
@@ -12,10 +12,16 @@ echo "Generating text"
 TEXT=$(./run.rb)
 
 echo "Getting token"
-TOKEN=$(curl -X POST -A 'User agent' -d "grant_type=password&username=$REDDIT_USERNAME&password=$REDDIT_PASSWORD" --user "$CLIENT_ID:$CLIENT_SECRET" https://www.reddit.com/api/v1/access_token | jq -r .access_token)
+TOKEN=$(
+  curl --silent --show-error --fail -X POST -A 'User agent' \
+  -d "grant_type=password&username=$REDDIT_USERNAME&password=$REDDIT_PASSWORD" \
+  --user "$CLIENT_ID:$CLIENT_SECRET" https://www.reddit.com/api/v1/access_token \
+  | jq -r .access_token
+)
 
 echo "Posting to reddit"
-curl -i -H "Authorization: Bearer $TOKEN" -A "User agent" -H "Content-Type: application/x-www-form-urlencoded" -X POST \
+curl --silent --output /dev/null --show-error --fail -i \
+  -H "Authorization: Bearer $TOKEN" -A "User agent" -H "Content-Type: application/x-www-form-urlencoded" -X POST \
   --data-urlencode "title=Starting hand of the week" \
   --data-urlencode "kind=self" \
   --data-urlencode "sr=$SUBREDDIT" \
