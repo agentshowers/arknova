4b08970ca57dd67aa70e307367893fdf8c7883ad
Joao Amorim <i.psyduck@gmail.com>
2024-04-15 16:14:05 +0100

    test script

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
-curl -X POST -A 'User agent' -d 'grant_type=password&username=<usr>&password=<pwd>' --user '<id>:<secret>' https://www.reddit.com/api/v1/access_token
+#!/bin/bash
 
-jq '.access_token'
+if [[ -z "${GITHUB_RUN_ID}" ]];then
+    echo "Not running on GitHub"
+else
+    echo "Running on GitHub"
+fi
+
+# curl -X POST -A 'User agent' -d 'grant_type=password&username=<usr>&password=<pwd>' --user '<id>:<secret>' https://www.reddit.com/api/v1/access_token
+
+# jq '.access_token'
 
