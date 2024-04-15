dadd0839446ea7faa89a90af7f4048b1dc947a95
Joao Amorim <i.psyduck@gmail.com>
2024-04-15 17:11:02 +0100

    steps description in script

--- script.sh
+++ script.sh
@@ -8,9 +8,13 @@ if [[ -z "${SUBREDDIT}" ]];then
     SUBREDDIT=test
 fi
 
-TOKEN=$(curl -X POST -A 'User agent' -d "grant_type=password&username=$REDDIT_USERNAME&password=$REDDIT_PASSWORD" --user "$CLIENT_ID:$CLIENT_SECRET" https://www.reddit.com/api/v1/access_token | jq -r .access_token)
+echo "Generating text"
 TEXT=$(./run.rb)
 
+echo "Getting token"
+TOKEN=$(curl -X POST -A 'User agent' -d "grant_type=password&username=$REDDIT_USERNAME&password=$REDDIT_PASSWORD" --user "$CLIENT_ID:$CLIENT_SECRET" https://www.reddit.com/api/v1/access_token | jq -r .access_token)
+
+echo "Posting to reddit"
 curl -i -H "Authorization: Bearer $TOKEN" -A "User agent" -H "Content-Type: application/x-www-form-urlencoded" -X POST \
   --data-urlencode "title=Starting hand of the week" \
   --data-urlencode "kind=self" \
