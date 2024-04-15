d4f641cfaa3b655167741e385f25f54a622fe6af
Joao Amorim <i.psyduck@gmail.com>
2024-04-15 16:59:30 +0100

    post

--- script.sh
+++ script.sh
@@ -4,7 +4,18 @@ if [[ -z "${GITHUB_RUN_ID}" ]];then
     source .env
 fi
 
-#TOKEN=$(curl -X POST -A 'User agent' -d "grant_type=password&username=$REDDIT_USERNAME&password=$REDDIT_PASSWORD" --user "$CLIENT_ID:$CLIENT_SECRET" https://www.reddit.com/api/v1/access_token | jq -r .access_token)
+if [[ -z "${SUBREDDIT}" ]];then
+    SUBREDDIT=test
+fi
 
-./run.rb > post.md
-cat post.md
+TOKEN=$(curl -X POST -A 'User agent' -d "grant_type=password&username=$REDDIT_USERNAME&password=$REDDIT_PASSWORD" --user "$CLIENT_ID:$CLIENT_SECRET" https://www.reddit.com/api/v1/access_token | jq -r .access_token)
+TEXT=$(./run.rb)
+
+curl -i -H "Authorization: Bearer $TOKEN" -A "User agent" -H "Content-Type: application/x-www-form-urlencoded" -X POST \
+  --data-urlencode "title=Testing again" \
+  --data-urlencode "kind=self" \
+  --data-urlencode "sr=$SUBREDDIT" \
+  --data-urlencode "resubmit=true" \
+  --data-urlencode "sendreplies=true" \
+  --data-urlencode "text=$TEXT" \
+  https://oauth.reddit.com/api/submit
