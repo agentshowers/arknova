b140c88a88064969a8626cb647e6476062431d3e
Joao Amorim <i.psyduck@gmail.com>
2024-04-15 16:21:26 +0100

    env secrets

--- script.sh
+++ script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 if [[ -z "${GITHUB_RUN_ID}" ]];then
-    echo "Not running on GitHub"
+    source .env
 else
     echo "Running on GitHub"
 fi
 
-# curl -X POST -A 'User agent' -d 'grant_type=password&username=<usr>&password=<pwd>' --user '<id>:<secret>' https://www.reddit.com/api/v1/access_token
+echo "Username: $REDDIT_USERNAME"
 
-# jq '.access_token'
-
+# curl -X POST -A 'User agent' -d "grant_type=password&username=$REDDIT_USERNAME&password=$REDDIT_PASSWORD" --user "$CLIENT_ID:$CLIENT_SECRET" https://www.reddit.com/api/v1/access_token | jq .access_token
